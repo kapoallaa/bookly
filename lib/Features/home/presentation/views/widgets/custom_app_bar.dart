54a39be0a5342ecbc4891ecbeb6157e671acec40
kapoallaa <kapoallaa@gmail.com>
2024-10-03 10:22:54 +0300

    add custom view app bar

---
+++ lib/Features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,26 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 62),
+      child: Row(
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 18,
+          ),
+          const Spacer(),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(FontAwesomeIcons.magnifyingGlass))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [CustomAppBar()],
+    );
+  }
+}
