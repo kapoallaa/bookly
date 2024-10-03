5c25848003cace99642170484340b0e0ea15aa78
kapoallaa <kapoallaa@gmail.com>
2024-10-03 16:00:27 +0300

    create app style

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -23,10 +23,34 @@ class HomeViewBody extends StatelessWidget {
           padding: EdgeInsets.only(left: 24),
           child: Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
           ),
-        )
+        ),
+        BestSellerListViewItem(),
       ],
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: AspectRatio(
+        aspectRatio: 2.5 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              image: const DecorationImage(
+                fit: BoxFit.fill,
+                image: NetworkImage(
+                    'https://upload.wikimedia.org/wikipedia/ar/9/98/%D8%A3%D8%B1%D8%B6_%D8%B2%D9%8A%D9%83%D9%88%D9%84%D8%A7.jpg'),
+              )),
+        ),
+      ),
     );
   }
 }
